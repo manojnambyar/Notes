2f1f72c18f0fd510c6361abdfc58bd88cb92f357
Manoj Bavakad <manojnambyar@gmail.com>
2021-12-01 08:52:33 +0530

    Custom Tool Bar

--- app/src/main/java/in/geofoods/notes/NoteListActivity.java
+++ app/src/main/java/in/geofoods/notes/NoteListActivity.java
@@ -1,22 +1,99 @@
 package in.geofoods.notes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import in.geofoods.notes.adapters.NotesRecyclerAdapter;
 import in.geofoods.notes.models.Note;
+import in.geofoods.notes.util.VerticalSpacingItemDecorator;
 
 public class NoteListActivity extends AppCompatActivity {
 
-    private static final String TAG = "Note List Activity";
+    private static final String TAG = "NoteListActivity";
+    // ui components
+    private RecyclerView mRecyclerView;
+
+    //variables
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NotesRecyclerAdapter mNotesRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_list);
+        Log.d(TAG, "onCreate: started");
+        mRecyclerView = findViewById(R.id.recyclerView);
 
-        Note note=new Note("some title","some content","some time stamp");
+        initRecyclerView();
+        insertFakeNotes();
 
-
+        setSupportActionBar((Toolbar) findViewById(R.id.notes_toolbar));
+        setTitle("Notes");
+        Log.d(TAG, "onCreate: ended");
     }
+
+    private void insertFakeNotes() {
+        Log.d(TAG, "insertFakeNotes: started");
+        for (int i = 0; i < 1000; i++) {
+            Note note = new Note();
+            note.setTitle("Title # " + i);
+            note.setContent("Content # " + i);
+            note.setTimestamp("Jan 2019");
+            mNotes.add(note);
+        }
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+        Log.d(TAG, "insertFakeNotes: ended");
+    }
+
+    private void initRecyclerView() {
+        Log.d(TAG, "initRecyclerView: started");
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        VerticalSpacingItemDecorator verticalSpacingItemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerView.addItemDecoration(verticalSpacingItemDecorator);
+        mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes);
+        mRecyclerView.setAdapter(mNotesRecyclerAdapter);
+        Log.d(TAG, "initRecyclerView: ended");
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/in/geofoods/notes/adapters/NotesRecyclerAdapter.java
+++ app/src/main/java/in/geofoods/notes/adapters/NotesRecyclerAdapter.java
@@ -13,37 +13,39 @@ import java.util.ArrayList;
 import in.geofoods.notes.R;
 import in.geofoods.notes.models.Note;
 
-public class NotesRecyclerAdapter extends RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder>{
-    private ArrayList<Note> mNotes = new ArrayList<>();
+public class NotesRecyclerAdapter extends RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder> {
+
+    private ArrayList<Note> mNotes ;
 
     public NotesRecyclerAdapter(ArrayList<Note> notes) {
-        this.mNotes = mNotes;
+        this.mNotes = notes;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_note_list_item,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_note_list_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.timestamp.setText(mNotes.get(position).getTimestamp());
+        holder.title.setText(mNotes.get(position).getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mNotes.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         TextView title, timestamp;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            title=itemView.findViewById(R.id.note_title);
-            timestamp=itemView.findViewById(R.id.note_timestamp);
+            this.title = itemView.findViewById(R.id.note_title);
+            this.timestamp = itemView.findViewById(R.id.note_timestamp);
         }
     }
 }
